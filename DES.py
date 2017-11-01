d7ce81a6c68cc2564211b438dda42f2da0f8f9e4
Ricky Cordero <rcordero16@yahoo.com>
2017-11-01 10:10:42 -0400

    Completed the DES function, added tests for functions, fixed duplicate value in pc2 table

--- DES.py
+++ DES.py
@@ -13,7 +13,7 @@ p = [16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 
 
 pc1 = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4]
 
-pc2 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
+pc2 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
 
 s1 = numpy.matrix("14 4 13 1 2 15 11 8 3 10 6 12 5 9 0 7;"
                   "0 15 7 4 14 2 13 1 10 6 12 11 9 5 3 8;"
@@ -62,9 +62,6 @@ s_boxes = [s1, s2, s3, s4, s5, s6, s7, s8]
 
 # converts a hexstring to a binary string
 def hexstring_to_binary_string(hexstring):
-    # num_of_bits = len(hexstring)
-    # bitstring = bin(int(hexstring, 16))[2:].zfill(16)
-    # return ' '.join(bitstring[i:i + 4] for i in range(0, 32, 4))
     op = ''
     for char in hexstring:
         op += hex_bin_dict[char]
@@ -80,57 +77,8 @@ def hex_to_binary(hexstring):
 def p_permute(c):
     op = ""
     for index in p:
-        op += c[index]
+        op += c[index-1]
     return op
-
-
-# expands the given 32-bit string into a 48 bit-string
-# by permuting the given string and adding 16 twice repeating bits
-def expand(r_i):
-    op = ""
-    j = 0
-    for i in range(j, len(r_i)):
-        op += r_i[e[i]]
-        i += 1
-        j = i
-
-    for i in range(j, 30):
-        j = i
-        op += r_i[e[i]]
-
-    return op
-
-
-# Given a 48-bit string partitions it into 8 6-bit substrings and for each substring returns a 4-bit string
-def s_box(b_i):
-
-    b1 = b_i[0:6]
-    b2 = b_i[6:12]
-    b3 = b_i[12:18]
-    b4 = b_i[18:24]
-    b5 = b_i[24:30]
-    b6 = b_i[30:36]
-    b7 = b_i[36:42]
-    b8 = b_i[42:48]
-
-    bs = [b1, b2, b3, b4, b5, b6, b7, b8]
-
-    for b_j in bs:
-        decimal_row = int(b_j[0]+b_j[5], 2)  # concatenate b_j_1 and b_j_6, then convert to binary
-        decimal_col = int(b_j[1:5], 2)  #
-        c = ""  # a 32-bit string
-        for i in range(8):
-            c += int(s_boxes[i][decimal_row][decimal_col], 2)  # find the value in the s-boxes table of s-matrices and convert to binary
-    return c
-
-
-# takes a 32-bit string, and a 48-bit round key, then outputs a 32-bit string
-def f(r_i, k):
-    a = expand(r_i)  # expands the 32-bit string to a 48-bit string
-    b = bitarray(a) ^ k  # xor the resultant 48-bit string with the 48-bit key
-    c = s_box(b)
-    p_c = p_permute(c)
-    return p_c
 
 
 # given a 64-bit binary keystring returns a permuted 56-bit binary keystring
@@ -149,17 +97,21 @@ def pc2_permute(keystring):
     return k
 
 
-# Given a 64-bit binary bitstring returns a permuted 64-bit binary bitstring using the ip table
+# given a 64-bit binary bitstring returns a permuted 64-bit binary bitstring using the ip table
 def ip_permute(bitstring):
     op = ""
     for index in ip:
+        # print(index)
         op += bitstring[index-1]
     return op
 
 
-# given a 56-bit binary key and an integer round number returns the current round key
-def curr_key(key, round):
-    pass
+# given a 64-bit binary bitstring returns a permuted 64-bit binary bitstring using the ip inverse table
+def ip_inv_permute(bitstring):
+    op = ""
+    for index in ip_inv:
+        op += bitstring[index-1]
+    return op
 
 
 # max bits > 0 == width of the value in bits (e.g., int_16 -> 16)
@@ -168,127 +120,348 @@ def rotate_left(val, r_bits, max_bits):
     return (val << r_bits%max_bits) & (2**max_bits-1) | \
     ((val & (2**max_bits-1)) >> (max_bits-(r_bits%max_bits)))
 
+
 # given a binary bitstring, circularly shifts it by the given shift length
 def shift_left(bitstring, shift_length):
     return bin(rotate_left(int(bitstring, 2), shift_length, len(bitstring)))[2:].zfill(len(bitstring))
 
-# generates the 24-bit left and right halves of the nth round key
-def gen_n(c, n):
-    if n == 16:
+
+# given the initial 24-bit left or right half, c_0 or d_0, generates the respective nth round half c_n, or d_n
+def generate_cn_or_dn(c, n):
+    if n == 0:
+        return c
+    else:
+        return generate_cn_or_dn_recurse(c, n, 0)
+
+
+# given the initial 24-bit left or right half, c_0 or d_0, generates the respective nth round half c_n, or d_n by
+# accumulating on the integer n which begins with value 1
+def generate_cn_or_dn_recurse(c, n, i):
+    if i == n:
+        return c
+    if i+1 == 16:
         shift_length = 1
         return shift_left(c, shift_length)
-    if n in [1,2,9]:
+    if i+1 in [1, 2, 9]:
         shift_length = 1
-        return gen_n(shift_left(c, shift_length), n+1)
+        return generate_cn_or_dn_recurse(shift_left(c, shift_length), n, i+1)
     else:
         shift_length = 2
-        return gen_n(shift_left(c, shift_length), n+1)
+        return generate_cn_or_dn_recurse(shift_left(c, shift_length), n, i+1)
+
+
+# returns the list of all half bitstrings for each round key prior to PC2 permuting
+def collect_cn_or_dn(c):
+    return [generate_cn_or_dn(c, n) for n in range(1, 17)]
+
 
 # given the initial left and right 28-bit binary strings, generates the key schedule list by permuting the
-# concatenation of the current left and right
+# concatenation of all left and right corresponding pairs
 def collect_keys(c0, d0):
-    round_keys = []
-    for n in range(1, 16):
-        c_n = gen_n(c0, n)  # passes the round as the second parameter
-        d_n = gen_n(d0, n)
-        k_n = pc2_permute(c_n+d_n)
-        round_keys.append(k_n)
+    c_n = collect_cn_or_dn(c0)  # evaluates each c_n into a list
+    d_n = collect_cn_or_dn(d0)  # evaluates each d_n into a list
+    round_keys = [pc2_permute(x[0]+x[1]) for x in zip(c_n, d_n)]  # permutes the concatenation of each respective c_n, d_n
     return round_keys
+
 
 # given the initial 64-bit key, converts it to a 56-bit binary string by permuting it under PC1, then
 # splits the result into two 28-bit binary strings, and generates the key schedule with those two strings
 def generate_keys(key):
-    pc1_k = pc1_permute(hexstring_to_binary_string(key))  # a permuted 56-bit binary keystring
-    c0 = pc1_k[0:28]  # a 28-bit binary keystring
-    d0 = pc1_k[28:56]  # a 28-bit binary keystring
+    pc1_k = pc1_permute(hexstring_to_binary_string(key))  # shortens the key into a permuted 56-bit binary string
+    c0 = pc1_k[0:28]  # a 28-bit binary keystring representing the left half
+    d0 = pc1_k[28:56]  # a 28-bit binary keystring representing the right half
     return collect_keys(c0, d0)
 
-def do_stuff(l, r, round_keys):
-    if len(round_keys)==0:
-        return l+r
+
+# given the initial 32-bit left and right permuted key string partitions and the round key schedule
+# performs 16 rounds of encryption and returns a 64-bit string
+def loop(l, r, round_keys):
+    def format_spaces(string):
+        op = ""
+        for i in range(0, len(string), 4):
+            op += string[i:i + 4] + " "
+        return op
+
+    if len(round_keys) == 0:
+        return ip_inv_permute(r+l)  # swap L16 with D16 returning a 64-bit string
     else:
+        print("Round " + str(17 - len(round_keys)) + ": " + format_spaces(str(round_keys[0])))
+
+
         l_n = r
         k = round_keys[0]
-        r_n = bitarray(l) ^ f(r, k) # xor
-        return do_stuff(l_n, r_n, round_keys[1:len(round_keys)])
+        r_n = (bitarray(l) ^ bitarray(f(r, k))).to01()  # xor the previous left with the result of the feistel network
+        return loop(l_n, r_n, round_keys[1:len(round_keys)])
+
+
+# expands the given 32-bit string into a 48 bit-string
+# by permuting the given string and adding 16 twice repeating bits
+def expand(r_i):
+    op = ""
+    j = 0
+    for i in range(j, len(r_i)):
+        op += r_i[e[i]-1]
+        i += 1
+        j = i
+
+    for i in range(j, 48):
+        j = i
+        op += r_i[e[i]-1]
+
+    return op
+
+
+# given a 48-bit string, partitions it into 8 6-bit substrings and for each substring returns a 4-bit string
+def s_box(b_i):
+
+    b1 = b_i[0:6]
+    b2 = b_i[6:12]
+    b3 = b_i[12:18]
+    b4 = b_i[18:24]
+    b5 = b_i[24:30]
+    b6 = b_i[30:36]
+    b7 = b_i[36:42]
+    b8 = b_i[42:48]
+
+    bs = [b1, b2, b3, b4, b5, b6, b7, b8]
+    # bs = [b_i[i:i+6] for i in range(0,48,6)]
+    s = ""  # a 32-bit string representing the concatenation S1(B1)S2(B2)...S8(B8) to be returned
+    for i in range(8):
+        decimal_row = int(bs[i][0] + bs[i][5], 2)  # concatenate b_j_1 and b_j_6, then converts the result to decimal number
+        decimal_col = int(bs[i][1:5], 2)  # concatenate b_j_2, b_j_3, b_j_4, b_j_5, then convert the result to decimal number
+        s += bin(s_boxes[i][decimal_row, decimal_col])[2:].zfill(4)  # find the value in the s-boxes table of s-matrices and convert to binary
+    return s
+
+
+# takes a 32-bit string, and a 48-bit round key, then outputs a 32-bit string
+def f(r_i, k):
+    a = expand(r_i)  # expands the 32-bit string to a 48-bit string using the e table
+    b = (bitarray(a) ^ bitarray(k)).to01()  # xor the resultant 48-bit string with the 48-bit key and convert to string
+    c = s_box(b)
+    p_c = p_permute(c)
+    return p_c
 
 
 # given a 64-bit hex plaintext string, and a 64-bit hex key, produces a 64-bit hex ciphertext
 def DES(plaintext, key):
     round_keys = generate_keys(key)
-    ip = ip_permute(hexstring_to_binary_string(plaintext)) # a 64-bit permutation of the given plaintext
+    ip = ip_permute(hexstring_to_binary_string(plaintext))  # a 64-bit permutation of the given plaintext
     l_0 = ip[0:32]  # left initial permuted 32-bit string
-    r_0 = ip[32:64] # right initial permuted 32-bit string
+    r_0 = ip[32:64]  # right initial permuted 32-bit string
 
-    x = do_stuff(l_0, r_0, round_keys)
-    for i in range(len(round_keys)):
+    x = loop(l_0, r_0, round_keys)
 
-
-
-    return
+    return x
 
 
-
-# given a 64-bit hex plaintext string, a 56-bit hex key, produces a 64-bit hex ciphertext
-def g(w, key, round):
-    return
-    # round_key = curr_key(hex_to_binary(key), round)
-    # left = hex_to_binary(w)[0:32]  # convert the left side of the 32-bit hex string to binary
-    # right = hex_to_binary(w)[32:64]  # convert the right side of the 32-bit hex string to binary
-    # key = hex_to_binary(key)  # convert the hex key to binary
-
-    # if round == 0:  # initial case
-    #     left = hex_to_binary(w)[0:32]  # convert the left side of the 32-bit hex string to binary
-    #     right = hex_to_binary(w)[32:64]  # convert the right side of the 32-bit hex string to binary
-    #     key = hex_to_binary(keys[round])  # convert the hex key to binary
-    #     init_permutation = ip_permute(left+right)  # permutes the concatenated left and right 32-bit binary strings of the given plaintext
-    #     left = init_permutation[0:32]  # stores this round's left 32-bit binary string
-    #     right = init_permutation[32:64]  # stores this round's right 32-bit binary string
-    #     op = bitarray(left) ^ f(right, key[round])
-    #     return DES(op, keys[round+1:len(keys)], round+1)
-    # elif round == 15:  # base case
-    #     pass
-    # else:
-    #     # new_left = init_permutation[0:32]  # stores this round's left 32-bit binary string
-    #     # new_right = init_permutation[32:64]  # stores this round's right 32-bit binary string
-    #     output = bitarray(left) ^ f(right, key[round])  # xor this round's left with the function output
-    #
-    #     final_left = output[0:32]  # stores this round's final left 32-bit binary string
-    #     final_right = output[32:64]  # stores this round's final right 32-bit binary string
-    #     return DES(final_left)
-
+# ----------------------------------------------------------------------------------------------------------------------
 
 x = "0123456789ABCDEF"
 
-k = "133457799BBCDFF1"
+k_original = "133457799BBCDFF1"
 
-
-def test_pc1_permute():
-    test_key_1 = "0001001100110100010101110111100110011011101111001101111111110001"
-
-    op_key_1 = "11110000110011001010101011110101010101100110011110001111"
-
-    # a = pc1_permute(test_key_1)
-    # print(test_key_1)
-    # print(len(test_key_1))
-    # print(op_key_1)
-    # print(len(op_key_1))
-
-    # print(a)
-    # print(len(a))
-    return pc1_permute(test_key_1) == op_key_1 # true
+k = "1122334455667788"
 
 
 def test_rotate_left():
     bit1 = "0101"
     intbit1 = int(bit1, 2)
-    print(bit1)
     bit2 = "1010"
     op = bin(rotate_left(intbit1, 1, 4))[2:].zfill(4)
     return op == bit2
 
-def test_pc2_permute():
-
-
 # print(test_rotate_left())
+
+
+def test_shift_left():
+    c1 = "0000000011110000110011001011"
+    result = shift_left(c1, 1)
+    expected = "0000000111100001100110010110"
+    return result == expected
+
+# print(test_shift_left())
+
+
+def test_hexstring_to_binary_string():
+    key = "1122334455667788"
+    result = hexstring_to_binary_string(key)
+    expected = "0001000100100010001100110100010001010101011001100111011110001000"
+    return result == expected
+
+# print(test_hexstring_to_binary_string())
+
+
+def test_halves():
+    key = "1122334455667788"
+    pc1 = pc1_permute(hexstring_to_binary_string(key))
+    result1 = pc1[0:28]
+    result2 = pc1[28:56]
+    expected1 = "1000000001111000011001100101"
+    expected2 = "0110011001111000100000000101"
+    return result1 == expected1 and result2 == expected2
+
+# print(test_halves())
+
+
+def test_pc1_permute():
+    key = "1122334455667788"
+    result = pc1_permute(hexstring_to_binary_string(key))
+    expected = "10000000011110000110011001010110011001111000100000000101"
+    return result == expected
+
+# print(test_pc1_permute())
+
+
+def test_pc2_permute():
+    # key = "1122334455667788"
+    key = "133457799BBCDFF1"
+    pc1 = pc1_permute(hexstring_to_binary_string(key))
+    c0 = pc1[0:28]
+    d0 = pc1[28:56]
+    cn = collect_cn_or_dn(c0)
+    dn = collect_cn_or_dn(d0)
+    cndn = [x[0]+x[1] for x in zip(cn, dn)]  # list of concatenated left and right halves, c_n+d_n for 1 <= n <= 16
+    keys = [pc2_permute(x) for x in cndn]  # the key schedule
+    print(cndn)
+    print(keys)
+
+
+# test_pc2_permute()
+
+def test_p_permute():
+    s = "01011100100000101011010110010111"
+    result = p_permute(s)
+    expected = "00100011010010101010100110111011"
+    return result == expected
+
+# print(test_p_permute())
+
+
+def test_collect_keys():
+    key = "1122334455667788"
+    pc1 = pc1_permute(hexstring_to_binary_string(key))
+    c0 = pc1[0:28]
+    d0 = pc1[28:56]
+    result = collect_keys(c0, d0)
+    # return result == expected
+    print(result)
+    print(type(result))
+    print(len(result))
+
+# test_collect_keys()
+
+
+def test_generate_cn_or_dn():
+    key = "1122334455667788"
+    pc1 = pc1_permute(hexstring_to_binary_string(key))
+    c0 = pc1[0:28]
+    d0 = pc1[28:56]
+
+    result1 = generate_cn_or_dn(c0, 16)
+    print(result1)
+    result2 = generate_cn_or_dn(d0, 16)
+    print(result2)
+
+    result3 = generate_cn_or_dn(c0, 15)
+    print(result3)
+    result4 = generate_cn_or_dn(d0, 15)
+    print(result4)
+
+    expected1 = "1000000001111000011001100101"  # c16
+    expected2 = "0110011001111000100000000101"  # d16
+    expected3 = "1100000000111100001100110010"  # c15
+    expected4 = "1011001100111100010000000010"  # d15
+    return result1 == expected1 and result2 == expected2 and result3 == expected3 and result4 == expected4
+
+# print(test_generate_cn_or_dn())
+
+
+def test_collect_cn_or_dn():
+    key = "1122334455667788"
+    pc1 = pc1_permute(hexstring_to_binary_string(key))
+    c0 = pc1[0:28]
+    d0 = pc1[28:56]
+    c_n = collect_cn_or_dn(c0)
+    d_n = collect_cn_or_dn(d0)
+    print(c_n)
+    print(len(c_n))
+    print(d_n)
+    print(len(d_n))
+
+# test_collect_cn_or_dn()
+
+
+def test_expand():
+    r_0 = "11110000101010101111000010101010"
+    print(len(r_0))
+    result = expand(r_0)
+    print(result)
+    print(len(result))
+    expected = "011110100001010101010101011110100001010101010101"
+    print(expected)
+    print(len(expected))
+    return result == expected
+
+# print(test_expand())
+
+
+def test_f():
+    r_0 = "11110000101010101111000010101010"
+    k1 = "000110110000001011101111111111000111000001110010"
+
+    e = bitarray(expand(r_0))
+    print(e)
+    k = bitarray(k1)
+    print(k)
+    result = (e ^ k).to01()
+    print(result)
+
+    expected = "011000010001011110111010100001100110010100100111"
+    print(expected)
+
+    return result == expected
+
+# print(test_f())
+
+
+def test_s_box():
+    r_0 = "11110000101010101111000010101010"
+    k1 = "000110110000001011101111111111000111000001110010"
+
+    e = bitarray(expand(r_0))
+    k = bitarray(k1)
+    xored = (e ^ k).to01()
+    print(xored)
+    result = s_box(xored)
+    expected = "01011100100000101011010110010111"
+    return result == expected
+
+# print(test_s_box())
+
+
+def DES_test():
+    x = "00AABBCCDDEEFF99"
+    key = "1122334455667788"
+    x_original = "0123456789ABCDEF"
+    k_original = "133457799BBCDFF1"
+    result_original = DES(x_original, k_original)
+    expected_original = "1000010111101000000100110101010000001111000010101011010000000101"
+    # result = DES(x, key)
+    # expected = ""
+    return result_original == expected_original
+
+# print(DES_test())
+
+
+def print_round_keys():
+
+    x_original = "0123456789ABCDEF"
+    k_original = "133457799BBCDFF1"
+    # result_original = DES(x_original, k_original)
+    x = "00AABBCCDDEEFF99"
+    key = "1122334455667788"
+    result = DES(x, key)
+
+
+print(print_round_keys())
 
